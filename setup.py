682dcc8ffa25b5c1e5378855f5b01fa3672252b3
Alexander Jones <happy5214@gmail.com>
2015-10-12 07:11:09 -0500

    Bump version number

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='competitions-match',
-    version='0.1',
+    version='0.1.1',
 
     description='competitions support library for matches',
     long_description=long_description,
